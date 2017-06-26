f3adf97304d20de7a6b77b77ccf6be5fe53d8757
eSklarzyk <emanuela@post.com>
2017-06-26 16:13:39 -0400

    changes

--- Scripts/UI/gameOver.js
+++ Scripts/UI/gameOver.js
@@ -1,46 +1,40 @@
 (function (window) {
     window.game = window.game || {};
+    //Global Variables
+    var canvas;
+    var stage;
+    var clickMeButton;
     function GameOver() {
         this.initialize();
     }
     var p = GameOver.prototype = new createjs.Container();
-    p.Container_initialize = p.initialize;
-    p.titleTxt = null;
-    p.count = 0;
-    p.initialize = function () {
+    this.Container_initialize = this.initialize;
+    this.titleTxt = null;
+    this.count = 0;
+    this.initialize = function () {
         this.Container_initialize();
         this.addBack();
         this.addOptions();
     };
-    p.addBack = function () {
+    this.addBack = function () {
         var bag = new createjs.Bitmap("./assets/gameOver.png");
         this.addChild(bag);
     };
-    p.addOptions = function () {
-        var option1, option2;
-        option1 = new ui.SimpleButton('Main Menu');
-        option1.on('click', this.mainMenu, this);
-        option1.regX = option1.width / 2;
-        option1.x = canvas.width / 2;
-        option1.y = 400;
-        option1.setButtonListeners({ color: '#FFF', borderColor: '#FFF',
-            overColor: '#900' });
-        this.addChild(option1);
-        option2 = new ui.SimpleButton('Play Game');
-        option2.on('click', this.plGame, this);
-        option2.regX = option2.width / 3;
-        option2.x = canvas.width / 3;
-        option2.y = 500;
-        option2.setButtonListeners({ color: '#FFF', borderColor: '#FFF',
-            overColor: '#900' });
-        this.addChild(option2);
+    this.addOptions = function () {
+        var option1;
+        option1 = new createjs.Bitmap("../Assets/Sprites/enemyUFO.png");
+        option1.on("click", clickMeButton_Click);
+        stage.addChild(option1);
     };
-    p.mainMenu = function (e) {
-        this.dispatchEvent(game.GameStateEvents.MAIN_MENU);
+    function clickMeButton_Click() {
+        this.mainMenu();
+    }
+    this.mainMenu = function (e) {
+        this.dispatchEvent(window.game.GameStateEvents.MAIN_MENU);
     };
-    p.plGame = function (e) {
-        this.dispatchEvent(game.GameStateEvents.GAME);
+    this.plGame = function (e) {
+        this.dispatchEvent(window.game.GameStateEvents.GAME);
     };
-    window.game.GameOver = GameOver;
+    return window.game.GameOver = GameOver;
 }(window));
 //# sourceMappingURL=gameOver.js.map
--- Scripts/UI/gameOver.ts
+++ Scripts/UI/gameOver.ts
@@ -1,55 +1,52 @@
 (function (window) {
-   window.game = window.game || {}
+  (<any>window).game =(<any>window).game || {}
+   
+   //Global Variables
+ var canvas:HTMLElement;
+  var stage:createjs.Stage;
+  var clickMeButton: createjs.Bitmap;
+
+   
    function GameOver() {
       this.initialize();
    }
    var p = GameOver.prototype = new createjs.Container();
-   p.Container_initialize = p.initialize;
+   this.Container_initialize = this.initialize;
    
-p.titleTxt = null;
-p.count = 0;
+this.titleTxt = null;
+this.count = 0;
 
 
-   p.initialize = function () {
+   this.initialize = function () {
       this.Container_initialize();
       this.addBack();
      this.addOptions();
    }
-   p.addBack = function () {
+   this.addBack = function () {
       var bag = new createjs.Bitmap("./assets/gameOver.png");
       this.addChild(bag);
 }
 
-p.addOptions = function () {
- var option1 , option2;
+this.addOptions = function () {
+ var option1;
+  option1 = new createjs.Bitmap("../Assets/Sprites/enemyUFO.png");
+    option1.on("click", clickMeButton_Click);
+    stage.addChild(option1);
+}
 
-option1 = new ui.SimpleButton('Main Menu');
-      option1.on('click',this.mainMenu,this);
-      option1.regX = option1.width / 2;
-      option1.x = canvas.width / 2;
-      option1.y = 400;
-      option1.setButtonListeners({color:'#FFF', borderColor:'#FFF', 
-      overColor:'#900'});
-      this.addChild(option1);
+function  clickMeButton_Click() {
+this.mainMenu();
+}
 
-      option2 = new ui.SimpleButton('Play Game');
-      option2.on('click',this.plGame,this);
-      option2.regX = option2.width / 3;
-      option2.x = canvas.width / 3;
-      option2.y = 500;
-      option2.setButtonListeners({color:'#FFF', borderColor:'#FFF', 
-      overColor:'#900'});
-      this.addChild(option2);
+   
+
+ this.mainMenu = function (e) {
+      this.dispatchEvent((<any>window).game.GameStateEvents.MAIN_MENU);
    }
 
-
- p.mainMenu = function (e) {
-      this.dispatchEvent(game.GameStateEvents.MAIN_MENU);
-   }
-
-    p.plGame = function (e) {
-      this.dispatchEvent(game.GameStateEvents.GAME);
+    this.plGame = function (e) {
+      this.dispatchEvent((<any>window).game.GameStateEvents.GAME);
    }
   
-   window.game.GameOver = GameOver;
+ return (<any>window).game.GameOver = GameOver;
 }(window));
