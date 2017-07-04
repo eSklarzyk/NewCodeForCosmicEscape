6d12d5ec5115ce22676c6a7875836e1dfe9b8cea
eSklarzyk <emanuela@post.com>
2017-07-04 16:26:32 -0400

    changes

--- Scripts/UI/gameStates.ts
+++ Scripts/UI/gameStates.ts
@@ -3,19 +3,31 @@
 
 (<any>window).game = (<any>window).game || {};
 
+
 let GameStates = {
 MAIN_MENU : 0,
 RUN_SCENE: 1,
 GAME:10,
-GAME_OVER:20
+GAME_OVER:20,
 }
 
 
 let GameStateEvents: {
     MAIN_MENU:  'main-menu-event';
     GAME_OVER: 'game-over-event';
-    GAME: 'game-event' ;
-    
+    GAME: 'game-event' ;  
+}
+let currentGameState = GameStates;
+
+function changeState (state) {
+
+switch (currentGameState){
+case 
+break;
+
+}
+
+
 }
 
 (<any>window).game.GameStates = GameStates;
